bca75815e6e713cc872213a13acfd4bc7f3ef904
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-08-29 20:38:53 +0300

    check commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
 
+using std::cout;
+using std::endl;
+
+class character {
+private:
+    std::string name;
+    short health;
+    short maxHealth;
+    short strength;
+    short agility;
+    short endurance;
+    short damage
+
+
+};
 int main() {
 
     return 0;
